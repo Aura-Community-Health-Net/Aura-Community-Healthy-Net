8557b8da9847a00055ff1c568ce8d5c13a49c7ee
R Saranjan <ragunathansaranjan@gmail.com>
2023-02-04 01:01:18 +0530

    doctor appointments page patially finished

--- controllers/DoctorAppointmentsController.php
+++ controllers/DoctorAppointmentsController.php
@@ -3,15 +3,26 @@
 namespace app\controllers;
 
 use app\core\Controller;
-use app\utilities\database;
+use app\core\database;
 
 class DoctorAppointmentsController extends Controller
 {
-    public static function getDoctorAppointmentsPage():array|bool|string{
 
-        return self::render(view:'doctor-dashboard-appointments', layout: "doctor-dashboard-layout", params: [],layoutParams: [
-            "title" => "New Appointments",
-            "active_link" => ""
+    public static function getDoctorAppointmentsPage():array|bool|string{
+        $nic = $_SESSION["nic"];
+        $providerType = $_SESSION["user_type"];
+
+        if (!$nic || $providerType != "doctor") {
+            header("location: /provider-login");
+            return "";
+        }
+
+        $db = new database();
+
+        return self::render(view: 'doctor-dashboard-appointments', layout: "doctor-dashboard-layout", params: [
+        ], layoutParams: [
+            "title" => "Appointments",
+            "active_link" => "appointments",
         ]);
 
     }
