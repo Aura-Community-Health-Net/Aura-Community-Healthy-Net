9e6276ef6daa4580b924af2f03b2fe7e55bc7287
R Saranjan <ragunathansaranjan@gmail.com>
2023-02-04 18:14:39 +0530

    feat:all the static pages finishes except dashboard

--- views/doctor-dashboard-appointments.php
+++ views/doctor-dashboard-appointments.php
@@ -15,20 +15,56 @@
                 <th>Mobile No</th>
                 <th>Location</th>
             </tr>
-            <tr>
-                <td><img src="<?php echo $doctor['profile_picture'] ?>"><?php echo $doctor["name"]; ?></td>
-                <td><?php echo $timeslots['from-time']; echo $timeslots['to-time']?></td>
-                <td></td>
-                <td></td>
-            </tr>
         </table>
+        <div class="doctor-appointments__left__data">
+            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
+                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
+                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="/assets/images/vector.png" style="width: 15px" >
+            </p>
+            <button style="background-color: #7ABCF1">Cancel</button>
+            <button style="background-color: #008DFF">Confirm</button>
+        </div>
+        <div class="doctor-appointments__left__data">
+            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
+                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
+                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="/assets/images/vector.png" style="width: 15px" >
+            </p>
+            <button style="background-color: #7ABCF1">Cancel</button>
+            <button style="background-color: #008DFF">Confirm</button>
+        </div>
+        <div class="doctor-appointments__left__data">
+            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
+                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
+                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="/assets/images/vector.png" style="width: 15px" >
+            </p>
+            <button style="background-color: #7ABCF1">Cancel</button>
+            <button style="background-color: #008DFF">Confirm</button>
+        </div>
+        <div class="doctor-appointments__left__data">
+            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
+                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
+                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="/assets/images/vector.png" style="width: 15px" >
+            </p>
+            <button style="background-color: #7ABCF1">Cancel</button>
+            <button style="background-color: #008DFF">Confirm</button>
+        </div>
     </div>
+
+
     <div class="doctor-appointments__right">
         <div class="doctor-appointments__right__top">
 
         </div>
         <div class="doctor-appointments__right__bottom">
-
+            <a href="https://www.google.com/maps"><img src="/assets/images/map.png"></a>
         </div>
     </div>
 </div>
--- views/doctor-dashboard-patients.php
+++ views/doctor-dashboard-patients.php
@@ -17,6 +17,37 @@
             <th class="doctor-patients__head">Last Checked</th>
             <th class="doctor-patients__head">Observation</th>
         </tr>
-
     </table>
+    <div class="doctor-patients__data">
+        <p><img src="/assets/images/profilepic2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Kamal Deshapriya &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          0762639672 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            28th of October 2022 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Lorem Ipsum is simply Lorem simply
+        </p>
+    </div>
+    <div class="doctor-patients__data">
+        <p><img src="/assets/images/profilepic2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Kamal Deshapriya &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0762639672 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            28th of October 2022 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Lorem Ipsum is simply Lorem simply
+        </p>
+    </div>
+    <div class="doctor-patients__data">
+        <p><img src="/assets/images/profilepic2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Kamal Deshapriya &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0762639672 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            28th of October 2022 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Lorem Ipsum is simply Lorem simply
+        </p>
+    </div>
+    <div class="doctor-patients__data">
+        <p><img src="/assets/images/profilepic2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Kamal Deshapriya &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            0762639672 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            28th of October 2022 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Lorem Ipsum is simply Lorem simply
+        </p>
+    </div>
 </div>
