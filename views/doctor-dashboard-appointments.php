f6deedd10620859bf822abc05fbe422defd707a0
R Saranjan <ragunathansaranjan@gmail.com>
2023-02-05 11:23:40 +0530

    feat: doctor dashboard page finished, full static pages finished

--- views/doctor-dashboard-appointments.php
+++ views/doctor-dashboard-appointments.php
@@ -20,7 +20,7 @@
             <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
                 Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
                 4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <img src="/assets/images/vector.png" style="width: 15px" >
             </p>
             <button style="background-color: #7ABCF1">Cancel</button>
@@ -28,9 +28,9 @@
         </div>
         <div class="doctor-appointments__left__data">
             <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
-                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                Kamala Dahanayake &nbsp; &nbsp; &nbsp; &nbsp;
+                3.45 pm - 4.45 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                076-2978546   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 <img src="/assets/images/vector.png" style="width: 15px" >
             </p>
             <button style="background-color: #7ABCF1">Cancel</button>
@@ -38,9 +38,9 @@
         </div>
         <div class="doctor-appointments__left__data">
             <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
-                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                Anoj Kumarathunga &nbsp; &nbsp; &nbsp; &nbsp;
+                5.00 pm - 5.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                076-2378678   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 <img src="/assets/images/vector.png" style="width: 15px" >
             </p>
             <button style="background-color: #7ABCF1">Cancel</button>
@@ -48,9 +48,9 @@
         </div>
         <div class="doctor-appointments__left__data">
             <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
-                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                Semini Peduruarachchi &nbsp; &nbsp; &nbsp; &nbsp;
+                5.10 pm - 5.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
+                076546472   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;
                 <img src="/assets/images/vector.png" style="width: 15px" >
             </p>
             <button style="background-color: #7ABCF1">Cancel</button>
@@ -61,7 +61,7 @@
 
     <div class="doctor-appointments__right">
         <div class="doctor-appointments__right__top">
-
+            <img src="/assets/images/Month.png">
         </div>
         <div class="doctor-appointments__right__bottom">
             <a href="https://www.google.com/maps"><img src="/assets/images/map.png"></a>
--- views/doctor-dashboard-feedback.php
+++ views/doctor-dashboard-feedback.php
@@ -12,7 +12,7 @@
     <div class="doctor-feedbacks__data">
         <img src="/assets/images/profilepic3.jpg">
            <h3><b>Kamal Deshapriya</b></h3>
-           <h4>26th of October 2022</h4>
+           <h4>22th of August 2022</h4>
         <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries, but also the leap into
@@ -23,8 +23,8 @@
 
     <div class="doctor-feedbacks__data">
         <img src="/assets/images/profilepic3.jpg">
-        <h3><b>Kamal Deshapriya</b></h3>
-        <h4>26th of October 2022</h4>
+        <h3><b>Sahan Kariyawasam</b></h3>
+        <h4>29th of September 2022</h4>
         <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries, but also the leap into
@@ -35,8 +35,8 @@
 
     <div class="doctor-feedbacks__data">
         <img src="/assets/images/profilepic3.jpg">
-        <h3><b>Kamal Deshapriya</b></h3>
-        <h4>26th of October 2022</h4>
+        <h3><b>Ravishi Palihawadana</b></h3>
+        <h4>28th of October 2022</h4>
         <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries, but also the leap into
