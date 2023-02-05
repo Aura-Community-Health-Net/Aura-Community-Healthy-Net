9360640bc18ac1876fe9e8e0035d49c556b3238e
Lahiru SWampath <2019CS150@stu.ucsc.cmb.lk>
2023-02-05 10:04:38 +0530

    Add the changes of care rider dash board

---
+++ views/care-rider-dashboard-analytics.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @var array $appointments
+ */
+?>
+
+<head>
+    <link rel="stylesheet" href="assets/css/pages/care-rider-dashboard-analytics.css">
+    <title>Analytics</title>
+</head>
+
+<div class="doctor-analytics">
+    <div class="doctor-analytics__top">
+        <img src="/assets/images/count.jpg">
+    </div>
+    <div class="doctor-analytics__bottom">
+        <img src="/assets/images/revenue.jpg">
+    </div>
+</div>
