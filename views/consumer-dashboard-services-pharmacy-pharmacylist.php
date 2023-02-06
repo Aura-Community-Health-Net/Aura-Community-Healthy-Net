7a50fa789f832d48ac5cd1cd890b8185fa1431e2
RavixEDMLover <ravipalihawadana0222@gmail.com>
2023-02-06 16:39:30 +0530

    consumer-med-list

--- views/consumer-dashboard-services-pharmacy-pharmacylist.php
+++ views/consumer-dashboard-services-pharmacy-pharmacylist.php
@@ -1,1 +1,72 @@
 <?php
+
+
+?>
+
+<div class="consumer-services-pharmacy-pharmacylist-Container">
+
+    <div class = "services-pharmacy-pharmacyList">
+        <div class="services-pharmacy-attributes" id="services-pharmacy-pharmacyName">
+            New Loyed Pharmacy
+        </div>
+
+        <div class= "services-pharmacy-attributes" id="services-pharmacy-pharmacyContactNo">
+            0706537572
+        </div>
+
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-pharmacyViewButton">
+            <button class="pharmacy-view-btn">view</button>
+        </div>
+
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-presUpload-btn">
+            <button class="pharmacy-pres-upload-btn">Upload prescription</button>
+        </div>
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-pharmacyLocation">
+            <i class="fa-solid fa-location-dot"></i>
+        </div>
+    </div>
+
+
+    <div class = "services-pharmacy-pharmacyList">
+        <div class="services-pharmacy-attributes" id="services-pharmacy-pharmacyName">
+            Health Mart Pharma
+        </div>
+
+        <div class= "services-pharmacy-attributes" id="services-pharmacy-pharmacyContactNo">
+            0716536672
+        </div>
+
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-pharmacyViewButton">
+            <button class="pharmacy-view-btn">view</button>
+        </div>
+
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-presUpload-btn">
+            <button class="pharmacy-pres-upload-btn">Upload prescription</button>
+        </div>
+        <div  class="services-pharmacy-attributes" id="services-pharmacy-pharmacyLocation">
+            <i class="fa-solid fa-location-dot"></i>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
