8746095a3a13767edac99ba6a73f76dcb173865a
RavixEDMLover <ravipalihawadana0222@gmail.com>
2023-02-07 16:48:38 +0530

    feat/consumer-services-pharmacy-payment-receipt-modifications

--- views/consumer-dashboard-services-pharmacy-payment-receipt.php
+++ views/consumer-dashboard-services-pharmacy-payment-receipt.php
@@ -4,55 +4,47 @@
 
 
 
-<div class="pharmacy-neworders-advanceinfo">
-    <div class="pharmacy-neworders-advanceinfo__consumer-details">
-        <div class="consumer-details__image">
-            <img class="consumer-profile-pic" src="/assets/images/profilepic1.jpg" alt="consumer-profile">
-        </div>
-        <div class="consumer-details__details">
-            <div class="consumer-details__details__name">
-                <p>Rovena Steinfield</p>
-            </div>
-            <div class="consumer-details__details__contact">
-                <p>070 45389032</p>
-            </div>
-        </div>
+<div class="consumer-pharmacy-neworders-advanceinfo">
+
+    <div class="consumer-pharmacy-neworders-advanceinfo-pharmacyName">
+       HealthMart Pharmacy
     </div>
-    <div class="pharmacy-neworders-advanceinfo__order-med-details">
-        <div class="pharmacy-neworders-advanceinfo__order-med-details__names">
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__names__name">
+
+    <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details">
+        <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__names">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__names__name">
                 <p>Available Medicines List</p>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__names__name">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__names__name">
                 <p>Total Amount</p>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__names__name">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__names__name">
                 <p>Advance Amount</p>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__names__name">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__names__name">
                 <p>Note</p>
             </div>
         </div>
-        <div class="pharmacy-neworders-advanceinfo__order-med-details__description">
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__description__description">
+        <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__description">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__description__description">
                 <ul class="medicines_list">
                     <li>Amoxicillin/clavulanic acid Tablet/Capsule</li>
                     <li>Piriton tablets</li>
                     <li>Omeprazole Capsules</li>
                 </ul>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__description__description">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__description__description">
                 <p>Rs.2790</p>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__description__description">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__description__description">
                 <p>Rs.1000</p>
             </div>
-            <div class="pharmacy-neworders-advanceinfo__order-med-details__description__description">
+            <div class="consumer-pharmacy-neworders-advanceinfo__order-med-details__description__description">
                 <p>vfevhcbhdbubidubbdbdhcbwhduhewff</p>
             </div>
         </div>
     </div>
-    <div class="pharmacy-neworders-advanceinfo__button">
-        <button class="advance-info-submit-button" id="advance-info-submit-button">send</button>
+    <div class="consumer-pharmacy-neworders-advanceinfo__button">
+        <button class="advance-info-submit-button" id="advance-info-submit-button">Continue to Pay</button>
     </div>
 </div>
