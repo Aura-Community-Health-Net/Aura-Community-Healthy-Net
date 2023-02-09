db160acef5b91e99c3fbd84c162e4008e64c43d8
R Saranjan <ragunathansaranjan@gmail.com>
2023-02-09 18:16:27 +0530

    feat: working process 6

--- views/consumer-dashboard-service-doctor.php
+++ views/consumer-dashboard-service-doctor.php
@@ -1,92 +1,120 @@
+<head>
+    <link rel="stylesheet" href="/assets/css/main.css">
+
+</head>
 <div class="consumer-dashboard-doctor">
-    <div class="consumer-dashboard-doctor-top">
+    <div class="consumer-dashboard-doctor__top">
         <form action="" class="form-item--search">
+
             <div class="search-bar">
-                <input type="text" placeholder="Search Doctor..." name="search">
+                <input type="text" placeholder="Search..." name="search">
                 <button type="submit"><i class="fa fa-search"></i></button>
             </div>
+
+            <select class="form-items--dropdown" name="product-categories" id="product-categories">
+                <option value="Doctor">Western Doctor</option>
+                <option value="Pharmacy">Indigenous Doctor</option>
+                <option value="Product Seller">Counsellor</option>
+            </select>
+
         </form>
-        <select class="form-items--dropdown" name="product-categories" id="product-categories">
-            <option value="Doctor">Doctor</option>
-            <option value="Pharmacy">Pharmacy</option>
-            <option value="Product Seller">Product Seller</option>
-            <option value="Care Rider">Care Rider</option>
-        </select>
     </div>
+    <div class="doctor__card-details">
+        <div class="doctor-container">
 
-    <div class="consumer-dashboard_bottom">
-
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic2.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Mr. Kamal Sahabandu</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span> :Car</p>
+                        <p><span>color </span> :Yellow</p>
+                        <p><span>Mobile No </span> :0762345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
             </div>
+
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic3.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Mr. Lahiru Sampath</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span>: Wagan r</p>
+                        <p><span>color </span> :Red</p>
+                        <p><span>Mobile No </span> :0712345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic4.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Miss. Ravishi Palihawadana</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span> :Aqua</p>
+                        <p><span>color </span> :White</p>
+                        <p><span>Mobile No </span>: 0752345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic5.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Mr. Anjana Dilshan</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span> :KDH Van</p>
+                        <p><span>color </span> :Black</p>
+                        <p><span>Mobile No </span> :0712345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic6.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Mr. Dhanuka Iroshana</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span> :Wagan r</p>
+                        <p><span>color </span> :White</p>
+                        <p><span>Mobile No </span> :0712345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="doctor-card">
+                <img src="/assets/images/profilepic7.jpg">
+                <div class="consumer-dashboard-doctor__bottom__center__data">
+                    <h2> Mr. Dumindu Lakshan</h2>
+                    <div>
+                        <p><span>Type of Vehicle </span>: Caraven</p>
+                        <p><span>color </span> :Brown</p>
+                        <p><span>Mobile No </span> :0712345678</p>
+                    </div>
+                    <a href="/consumer-dashboard/services/doctor/profile">
+                        <button > Request</button>
+                    </a>
+                </div>
+            </div>
+
         </div>
 
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
-            </div>
-        </div>
-
-
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
-            </div>
-        </div>
-
-
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
-            </div>
-        </div>
-
-
-
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
-            </div>
-        </div>
-
-
-        <div class="consumer-dashboard-doctor__profile__details">
-            <img src="/assets/images/profilepic1.jpg">
-            <div class="consumer-dashboard-doctor_details">
-                <h3><b>Prof. Anoja Abeyjeewa</b></h3>
-                <h4>Anaesthetists</h4>
-                <a href="/consumer-dashboard/services/doctor/profile">
-                    <button>Make Appointment</button>
-                </a>
-            </div>
-        </div>
-    </div>
-
-
-</div>
