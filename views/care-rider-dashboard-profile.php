1f5c02eedd7921aa23f11202daf2df9893594372
Lahiru SWampath <2019CS150@stu.ucsc.cmb.lk>
2023-02-09 14:35:54 +0530

    feat/care-rider-dashboard

--- views/care-rider-dashboard-profile.php
+++ views/care-rider-dashboard-profile.php
@@ -1,29 +1,18 @@
 <head><link rel="stylesheet" href="/assets/css/main.css"></head>
 
+<div class="care-rider-profile-pic__container">
+    <div class="care-rider-profile-pic__header">
+        <img src="/assets/images/profilepic2.jpg" alt="">
+        <div>
+            <h1>Lasith Lakshan</h1>
+            <h2>Care Rider</h2>
+        </div>
+    </div>
 
-<div class="care-rider-profile-contener">
-    <div class="care-rider-profile-content">
-        <div class="care-rider-profile-content-head">
-            <img class="care-rider-profile-image" src="/assets/images/care-rider-profile.jpg">
-            <h1>Dasuni wimlasuriya arachchi</h1>
-
-        </div>
-
-        <div class="care-rider-profile-content-body">
-            <h3>Email Address</h3>
-            <h3 >dasuni@gmail.com</h3>
-        </div>
-        <div class="care-rider-profile-content-body">
-            <h3>NIC</h3>
-            <h3 >457563212V</h3>
-        </div>
-        <div class="care-rider-profile-content-body">
-            <h3>Mobile Number      071-2345671</h3>
-<!--            <h3 >071-2345671</h3>-->
-        </div>
-        <div class="care-rider-profile-content-body">
-            <h3>Address</h3>
-            <h3 >No:1,Thelwatta,Ambalangoda</h3>
-        </div>
+    <div class="care-rider-profile-pic__details">
+        <h3> <span>Email Address</span>  lasith@gmail.com</h3>
+        <h3><span>NIC</span>   3445545895V</h3>
+        <h3><span>Mobile Number</span>  0758578345</h3>
+        <h3><span>Address</span>  No:1,Thelwatta,Ambalangoda</h3>
     </div>
 </div>
--- views/care-rider-dashboard.php
+++ views/care-rider-dashboard.php
@@ -1,96 +1,96 @@
 <!DOCTYPE html>
 <html>
 
-<head>
     <link rel="stylesheet" href="/assets/css/main.css">
-
-</head>
-
-
-<div class="care-rider-contener-1">
-    <div class="care-rider-contener-top-1">
-        <div class="care-rider-sub-top-1" id="sup-scroll">
-            <h3>New Requests List</h3>
-            <?php
-            for ($x = 0; $x <= 3; $x++) {  ?>
-                <div class="care-rider-new-request-content-1">
-                    <img src="./assets/images/care-rider-dashboard-1.jfif">
-                    <div class="care-rider-new-request-content-details-1">
-                        <h3>Dasuni</h3>
-                        <h3>071-9988765</h3>
-                    </div>
-                    <div class="care-rider-location-icon-1"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
-                </div>
-            <?php } ?>
-
-        </div>
-        <div class="care-rider-sub-top-1">
-            <h3>Request </h3>
-            <div class="care-rider-request-details-1">
-
-                <div class="care-rider-request-details-head-1">
-                    <img src="./assets/images/care-rider-dashboard-1.jfif">
-                    <div class="care-rider-request-details-head-htag-1">
-                        <h3>Dasuni</h3>
-                        <h3>071-9988765</h3>
-                    </div>
-                </div>
-                <div class="care-rider-request-details-body-1">
-                    <h3>Date</h3>
-                    <h3>February 4, 2023</h3>
-                </div>
-                <div class="care-rider-request-details-body-1">
-                    <h3>Time</h3>
-                    <h3>1:12 PM</h3>
-                    <i class="fa fa-map-marker" aria-hidden="true"></i>
-                </div>
+<div class="dashboard__top-container">
+    <div class="dashboard__top-cards">
+        <h3>New Orders</h3>
+        <div class="dashboard__top-cards__detail">
+            <img class="order-consumer-img" src="/assets/images/profilepic1.jpg" alt="">
+            <div>
+                <h4>Kamal Deshapriya</h4>
+                <h5>Nil Veralu</h5>
             </div>
         </div>
-        <div class="care-rider-sub-top-1">
-            <h3> Requests count</h3>
-            <div class="care-rider-request-count-1">
-                <div class="care-rider-request-count-class-1">
-                    <h3>New Requests</h3>
-                    <span class="care-rider-all-1">112</span>
-                </div>
-                <div class="care-rider-request-count-class-1">
-                    <h3>All Requests</h3>
-                    <span class="care-rider-all-1">124</span>
-                </div>
 
+        <div class="dashboard__top-cards__detail">
+            <img class="order-consumer-img" src="/assets/images/profilepic2.jpg" alt="">
+            <div>
+                <h4>Semini Peduruarachchi</h4>
+                <h5>Cashews</h5>
+            </div>
+        </div>
+
+        <div class="dashboard__top-cards__detail">
+            <img class="order-consumer-img" src="/assets/images/profilepic3.jpg" alt="">
+            <div>
+                <h4>Anoj Karunarathna</h4>
+                <h5>Vegetable Porridge</h5>
+            </div>
+        </div>
+
+        <div class="dashboard__top-cards__detail">
+            <img class="order-consumer-img" src="/assets/images/profilepic4.jpg" alt="">
+            <div>
+                <h4>Kamal Disasekara</h4>
+                <h5>Pachchaperumaal</h5>
             </div>
         </div>
     </div>
-    <div class="care-rider-contener-bottom-1">
-        <div class="care-rider-sub-map-1">
-            <h3>Google Map</h3>
-            <div class="care-rider-mapouter-1">
-                <div class="care-rider-gmap_canvas-1">
-                    <iframe class="care-rider-gmap_iframe-1" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=800&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 
+    <div class="dashboard__top-cards">
+        <h3>Orders</h3>
+        <div class="dashboard__top-cards__info">
+            <div class="dashboard__top-cards__detail">
+                <img class="order-consumer-img" src="/assets/images/profilepic1.jpg" alt="">
+                <div>
+                    <h4>Kamal Deshapriya</h4>
+                    <h5>0726778767</h5>
                 </div>
-
             </div>
 
+            <div class="care-rider-order-details">
+                <div>
+                    <h5>Date</h5>
+                    <h5>9th October 2023 </h5>
+                </div>
+                <div>
+                    <h5>Time</h5>
+                    <h5>12.00 pm</h5>
+                </div>
+            </div>
         </div>
-        <div class="care-rider-sub-analystic-1">
-            <h3>Analystic</h3>
 
-            <div class="care-rider-analystic-1">
-                <div class="care-rider-analystic-class-1">
-                    <img src="./assets/images/care-rider-dashboard-2.jfif">
 
-                </div>
-                <div class="care-rider-analystic-class-1">
-                    <p>
-                        The meaning of ANALYTIC is
-                        especially : separating something into component parts or constituent elements.
-                    </p>
 
-                </div>
+    </div>
 
-            </div>
+    <div class="dashboard__top-cards">
+        <h3>Order Count</h3>
+        <div class="order-count__details">
+            <h3>New Orders</h3>
+            <p class="new-order-count">15</p>
+        </div>
+        <div class="order-count__details">
+            <h3>All Orders</h3>
+            <p class="all-order-count">59</p>
         </div>
     </div>
 </div>
 
+<div class="dashboard__bottom-container">
+    <div class="dashboard__bottom-cards">
+        <h3>New Orders</h3>
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d270201.1012553059!2d80.57066973934896!3d7.435740318327426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1668671876514!5m2!1sen!2slk"
+                    width="400" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0"></iframe>   </div>
+    </div>
+    <div class="dashboard__bottom-cards">
+        <h3>Analytics</h3>
+        <img class="dashboard-analytics-img" src="/assets/images/dashboard-analytics.jpg" alt="">
+        <div class="dashboard-analytics-description">
+            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the </p>
+        </div>
+    </div>
+</div>
