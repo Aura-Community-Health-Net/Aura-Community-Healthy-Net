ab8310092cdd304eb627a3242db9665a29132407
R Saranjan <ragunathansaranjan@gmail.com>
2023-02-09 10:04:36 +0530

    feat: working process

--- views/doctor-dashboard-appointments.php
+++ views/doctor-dashboard-appointments.php
@@ -6,67 +6,75 @@
  */
 ?>
 
-<div class="doctor-appointments">
-    <div class="doctor-appointments__left">
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Time Slot</th>
-                <th>Mobile No</th>
-                <th>Location</th>
-            </tr>
-        </table>
-        <div class="doctor-appointments__left__data">
-            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Kamal Deshapriya &nbsp; &nbsp; &nbsp; &nbsp;
-                4.00 pm - 4.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                0762639672   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <img src="/assets/images/vector.png" style="width: 15px" >
-            </p>
-            <button style="background-color: #7ABCF1">Cancel</button>
-            <button style="background-color: #008DFF">Confirm</button>
-        </div>
-        <div class="doctor-appointments__left__data">
-            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Kamala Dahanayake &nbsp; &nbsp; &nbsp; &nbsp;
-                3.45 pm - 4.45 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                076-2978546   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                <img src="/assets/images/vector.png" style="width: 15px" >
-            </p>
-            <button style="background-color: #7ABCF1">Cancel</button>
-            <button style="background-color: #008DFF">Confirm</button>
-        </div>
-        <div class="doctor-appointments__left__data">
-            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Anoj Kumarathunga &nbsp; &nbsp; &nbsp; &nbsp;
-                5.00 pm - 5.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                076-2378678   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                <img src="/assets/images/vector.png" style="width: 15px" >
-            </p>
-            <button style="background-color: #7ABCF1">Cancel</button>
-            <button style="background-color: #008DFF">Confirm</button>
-        </div>
-        <div class="doctor-appointments__left__data">
-            <p><img src="/assets/images/profilepic1.jpg">&nbsp;&nbsp;
-                Semini Peduruarachchi &nbsp; &nbsp; &nbsp; &nbsp;
-                5.10 pm - 5.30 pm &nbsp; &nbsp; &nbsp; &nbsp;
-                076546472   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;
-                <img src="/assets/images/vector.png" style="width: 15px" >
-            </p>
-            <button style="background-color: #7ABCF1">Cancel</button>
-            <button style="background-color: #008DFF">Confirm</button>
-        </div>
-    </div>
+<div class="doctor-appointments_container">
+        <div class="doctor-appointments__left__container">
+            <div class="doctor-appointments__left">
+                <div class="doctor-appointments__left__data">
+                    <img src="/assets/images/profilepic1.jpg">
+                    <p>Kamal Deshapriya</p>
+                    <p>4.00pm-4.30pm</p>
+                    <p>0762639672  </p>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+
+                <button style="background-color: #7ABCF1">Cancel</button>
+                <button style="background-color: #008DFF">Confirm</button>
+            </div>
 
 
-    <div class="doctor-appointments__right">
-        <div class="doctor-appointments__right__top">
-            <img src="/assets/images/Month.png">
+
+
+            <div class="doctor-appointments__left">
+                <div class="doctor-appointments__left__data">
+                    <img src="/assets/images/profilepic1.jpg">
+                    <p>Kamal Deshapriya</p>
+                    <p>4.00 pm - 4.30 pm </p>
+                    <p>0762639672  </p>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+
+                <button style="background-color: #7ABCF1">Cancel</button>
+                <button style="background-color: #008DFF">Confirm</button>
+            </div>
+
+
+            <div class="doctor-appointments__left">
+                <div class="doctor-appointments__left__data">
+                    <img src="/assets/images/profilepic1.jpg">
+                    <p>Kamal Deshapriya</p>
+                    <p>4.00 pm - 4.30 pm </p>
+                    <p>0762639672  </p>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+
+                <button style="background-color: #7ABCF1">Cancel</button>
+                <button style="background-color: #008DFF">Confirm</button>
+            </div>
+
+
+            <div class="doctor-appointments__left">
+                <div class="doctor-appointments__left__data">
+                    <img src="/assets/images/profilepic1.jpg">
+                    <p>Kamal Deshapriya</p>
+                    <p>4.00 pm - 4.30 pm </p>
+                    <p>0762639672  </p>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+
+                <button style="background-color: #7ABCF1">Cancel</button>
+                <button style="background-color: #008DFF">Confirm</button>
+            </div>
         </div>
-        <div class="doctor-appointments__right__bottom">
-            <a href="https://www.google.com/maps"><img src="/assets/images/map.png"></a>
+
+
+        <div class="doctor-appointments__right">
+            <div class="doctor-appointments__right__top">
+                <img src="/assets/images/Month.png">
+            </div>
+            <div class="doctor-appointments__right__bottom">
+                <a href="https://www.google.com/maps"><img src="/assets/images/map.png"></a>
+            </div>
         </div>
-    </div>
 </div>
 
 
