576cb1d1e45225a2c52cf9731c7444da67e90342
Lahiru SWampath <2019CS150@stu.ucsc.cmb.lk>
2023-04-27 15:20:28 +0530

    feat: sent

---
+++ views/consumer-dashboard-care-rider-request-sent.php
@@ -0,0 +1,8 @@
+<head>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Consumer</title>
+</head>
+<div class="doctor-profile__background">
+        <div class="doctor-profile__data">
+        </div>
+</div>
