6796d81a6de8a3903c4acd5b2a15a78c3f6af489
Randima Dias <randimamethminid525@gmail.com>
2023-05-01 13:19:00 +0530

    feat: implemented product seller analytic part

--- public/index.php
+++ public/index.php
@@ -91,6 +91,7 @@ $app->router->get('/product-seller-dashboard/profile', [ProfileController::class
 $app->router->get('/product-seller-dashboard/analytics', [AnalyticsController::class, 'getProductSellerAnalyticsPage']);
 $app->router->get('/product-seller-dashboard/analytics/revenue-chart', [AnalyticsController::class, 'getProductSellerAnalyticsRevenueChart']);
 $app->router->get('/product-seller-dashboard/analytics/order-count-chart', [AnalyticsController::class, 'getProductSellerAnalyticsOrderCount']);
+$app->router->get('/product-seller-dashboard/analytics/product-vs-revenue-chart', [AnalyticsController::class, 'getProductSellerRevenueVsProductPercentage']);
 
 // For pharmacy
 $app->router->get('/pharmacy-dashboard', [DashboardController::class, 'getPharmacyDashboard']);
